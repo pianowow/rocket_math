d4e39824b1b1e53e8f6f34faaceba463eeee776e
Chris Irwin <pianowow@gmail.com>
2025-08-10 21:41:57 -0400

    refactor for readability

--- curses_colors.py
+++ curses_colors.py
@@ -7,7 +7,7 @@ import curses
 def do_it(stdscr):
     #curses.echo()
     max_line, max_col = curses.LINES-1, curses.COLS-1
-    #w_title_bar = curses.newwin(1,max_col,0,0) #height, width, starty, startx
+    #w_title_bar = curses.newwin(1,max_col,0,0) #height, width, start_y, start_x
     # Create a custom color set that you might re-use frequently
     # Assign it a number (1-255), a foreground, and background color.
     # curses.init_pair(1, curses.COLOR_BLUE, curses.COLOR_MAGENTA)
@@ -24,21 +24,21 @@ def do_it(stdscr):
     # COLOR_WHITE 	    7
 
     # initialize all color pairs (1-64)
-    # background * 8 + foreground 
-    wins = []
+    # background * 8 + foreground
+    windows = []
     width = max_col // 8
     height = max_line // 8
-    for bg in range(8):
-        wins.append([])
-        starty = height*bg
-        for fg in range(8):
-            color = bg*8+fg+1
-            curses.init_pair(bg*8+fg+1, fg, bg )
-            startx = width*fg            
-            wins[bg].append(curses.newwin(height, width, starty, startx))
-            wins[bg][fg].addstr(0,0,("0"*(width-1)+"\n")*(height-1),curses.color_pair(color))
-            wins[bg][fg].refresh()
-    wins[0][0].getch()
+    for background_color in range(8):
+        windows.append([])
+        start_y = height*background_color
+        for foreground_color in range(8):
+            color = background_color*8+foreground_color+1
+            curses.init_pair(color, foreground_color, background_color)
+            start_x = width*foreground_color
+            windows[background_color].append(curses.newwin(height, width, start_y, start_x))
+            windows[background_color][foreground_color].addstr(0,0,("0"*(width-1)+"\n")*(height-1),curses.color_pair(color))
+            windows[background_color][foreground_color].refresh()
+    windows[0][0].getch()
 
 if __name__ == '__main__':
     curses.wrapper(do_it)
